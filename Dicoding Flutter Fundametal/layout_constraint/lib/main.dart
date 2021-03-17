605e5ad37f96bdcf487d91d79bcca7b15061f604
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2021-03-17 16:12:02 +0700

    update

--- Dicoding Flutter Fundametal/layout_constraint/lib/main.dart
+++ Dicoding Flutter Fundametal/layout_constraint/lib/main.dart
@@ -5,16 +5,21 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ConstrainedBox(
-        constraints: BoxConstraints(
-          minWidth: 70,
-          minHeight: 70,
-          maxWidth: 150,
-          maxHeight: 150,
-        ),
-        child: Container(
-          color: Colors.red,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minWidth: 70,
+              minHeight: 70,
+              maxWidth: 150,
+              maxHeight: 150,
+            ),
+            child: Container(
+              color: Colors.red,
+            ),
+          ),
         ),
       ),
     );
